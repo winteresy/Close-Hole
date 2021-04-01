cfdd1f00fc74e8344e7fd16f51f4e1499946085f
winteresy <alisagurova2@gmail.com>
2021-04-01 15:10:40 +0300

    master

---
+++ dynamic.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $(".hole_1").on("click", function(event) {
+  $(".hole_background").css("background-image", "url(images/1hole.jpg)");
+  });
+  $(".hole_2").on("click", function(event) {
+  $(".hole_background").css("background-image", "url(images/2hole.jpg)");
+  });
+  $(".hole_3").on("click", function(event) {
+  $(".hole_background").css("background-image", "url(images/3hole.png)");
+  });
+  $(".hole_4").on("click", function(event) {
+  $(".hole_background").css("background-image", "url(images/4hole.png)");
+  });
+});
